56e8fb12a64f06877e12ded6d9993044c13525f5
Louis Riehl <louisriehl@gmail.com>
2018-11-30 11:54:46 -0500

    joins now work correctly

--- server/app/models/klass.rb
+++ server/app/models/klass.rb
@@ -1,2 +1,4 @@
 class Klass < ApplicationRecord
+    has_many :spell_klasses
+    has_many :spells, through: :spell_klasses
 end
--- server/app/models/spell.rb
+++ server/app/models/spell.rb
@@ -1,2 +1,4 @@
 class Spell < ApplicationRecord
+    has_many :spell_klasses
+    has_many :klasses, through: :spell_klasses
 end
