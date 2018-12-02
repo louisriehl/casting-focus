201f843c611b88167abcc42e6f1806746956c91b
Louis Riehl <louisriehl@gmail.com>
2018-12-01 19:25:45 -0500

    successfully pulling json file

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Import spell data from JSON file to seed DB
+
+spell_list = JSON.parse(File.read('db/spells.json'))
+
+puts spell_list[1]
